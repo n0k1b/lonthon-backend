cfa5ac493d9b295102f36d893c4ceb1a23e66013
Sany655 <mazharulalam26@gmail.com>
2023-03-08 21:32:58 +0600

    business settings api complete

---
+++ database/factories/CategorySubcategoryGenreMapFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CategorySubcategoryGenreMapFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => factory(App\Models\Category::class)->create()->id,
+            'sub_category_id' => factory(App\Models\SubCategory::class)->create()->id,
+            'genre_id' => factory(App\Models\Genre::class)->create()->id,
+        ];
+    }
+}
