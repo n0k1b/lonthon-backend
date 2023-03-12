c47e8e18b5e715f530991a941c1b40cf143b5e8a
Sany655 <mazharulalam26@gmail.com>
2023-03-13 01:02:03 +0600

    update business setting api in convinient format

--- app/Http/Controllers/BusinessSettingController.php
+++ app/Http/Controllers/BusinessSettingController.php
@@ -9,6 +9,16 @@ class BusinessSettingController extends Controller
 {
     public function index()
     {
+        try {
+            $data = BusinessSetting::all();
+            if($data){
+                return $this->successJsonResponse("Business settings data found",$data);
+            }else{
+                return $this->errorJsonResponse("Business settings data not found");
+            }
+        } catch (\Throwable $th) {
+            return $this->exceptionJsonResponse($th);
+        }
         return json_decode(BusinessSetting::all());
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,73 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
+    use ValidatesRequests;
+    use ValidatesRequests;
+
+    /**
+     * Success JSON Response Method.
+     *
+     * @param string $message
+     * @param null $data
+     * @param null $total
+     * @return JsonResponse
+     */
+    public function successJsonResponse(string $message, $data = null, $total = null): JsonResponse
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data ?: [],
+            'total' => $total ?: ((is_null($data) ? 0 : (is_array($data) ? count($data) : 1))),
+        ]);
+    }
+
+    /**
+     * Error JSON Response Method.
+     *
+     * @param string $message
+     * @param array $data
+     * @return JsonResponse
+     */
+    public function errorJsonResponse(string $message, array $data = []): JsonResponse
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message ?: 'Something went wrong! Please try again later.',
+            'data' => $data,
+            'total' => 0,
+        ]);
+    }
+
+    /**
+     * Exception JSON Response.
+     *
+     * @param $exception
+     * @param string|null $message
+     * @param string $channel
+     * @return JsonResponse
+     */
+    public function exceptionJsonResponse($exception, string $message = null, string $channel = 'default'): JsonResponse
+    {
+        if ($channel === 'default') {
+            //Slack notification
+            //User::first()->notify(new DefaultNotification($exception));
+        }
+
+        // Response
+        return response()->json([
+            'status' => false,
+            'message' => $message ?? 'Something went wrong! Please try again later.',
+            'data' => [
+                'exceptions' => $exception->getMessage(),
+            ],
+            'total' => 0,
+        ]);
+    }
 }
