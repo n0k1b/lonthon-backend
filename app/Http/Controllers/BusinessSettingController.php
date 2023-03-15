b1e1461f637c887fd41632fa852085891deac7e8
nokib-baybit <nokib@baybit.com>
2023-03-15 23:55:48 +0600

    Business settings api updated

--- app/Http/Controllers/BusinessSettingController.php
+++ app/Http/Controllers/BusinessSettingController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\BusinessSetting;
 
 class BusinessSettingController extends Controller
 {
     public function index()
     {
-        return json_decode(BusinessSetting::all());
+        try {
+            $data = BusinessSetting::all();
+            if ($data) {
+                return $this->successJsonResponse("Business settings data found", $data);
+            } else {
+                return $this->errorJsonResponse("Business settings data not found");
+            }
+        } catch (\Throwable$th) {
+            return $this->exceptionJsonResponse($th);
+        }
     }
 }
