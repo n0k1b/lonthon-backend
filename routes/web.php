e2ab8172d20ed3b05d32eeec9f76857374706e37
nokib-baybit <nokib@baybit.com>
2023-05-07 18:26:20 +0600

    Merged with ln-20

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+    Route::get('/',function () { return view("admin.dashboard"); });
+
+
+    // contents
+    Route::get('/content',[ContentController::class,"show"]);
+    Route::get('/create-cont',[ContentController::class,"create"]);
+    Route::prefix('/content')->group(function () {
+        Route::post('/insert', [ContentController::class, "insert"])->name("content-inserting");
+        Route::get('/delete/{id}', [ContentController::class, "delete"])->name("content-deleting");
+        Route::get('/edit/{id}', [ContentController::class, "edit"])->name("content-editing");
+        Route::post('/update/{id}', [ContentController::class, "update"])->name("content-updating");
+        // Route::get('/trash', [ContentController::class, "trash"]);
+        // Route::get('/restore/{id}', [ContentController::class, "restore"])->name("content-restore");
+        // Route::get('/forced/{id}', [ContentController::class, "forced"])->name("content-forced");
+    });
 
 require __DIR__.'/auth.php';
