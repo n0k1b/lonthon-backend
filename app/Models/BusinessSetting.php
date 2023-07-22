7c33f9e1682386654406d766d29eadfe5b675cf4
nokib-baybit <nokib@baybit.com>
2023-07-23 00:44:54 +0600

    BusinessSettings updated

--- app/Models/BusinessSetting.php
+++ app/Models/BusinessSetting.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class BusinessSetting extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    public function getHomepageBannerImageAttribute($value)
+    {
+        return env('APP_URL') . 'storage/' . $value;
+    }
+
+    public function getHomepagePromotionalBanner1Attribute($value)
+    {
+        return env('APP_URL') . 'storage/' . $value;
+    }
+
+    public function getHomepagePromotionalBanner2Attribute($value)
+    {
+        return env('APP_URL') . 'storage/' . $value;
+    }
+
+    public function getLogoAttribute($value)
+    {
+        return env('APP_URL') . 'storage/' . $value;
+    }
+
+    public function getFaviconAttribute($value)
+    {
+        return env('APP_URL') . 'storage/' . $value;
+    }
 }
