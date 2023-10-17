ab88af0a8274e27f2dfd14b71cb55f790014a24b
nokib-baybit <nokib@baybit.com>
2023-10-18 02:18:51 +0600

    pdf content updated

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -92,15 +92,14 @@ class ContentController extends Controller
                     'media_text' => $request->content,
                 ];
             } elseif ($request->content_type == 1) {
+                $contentFiles = $request->file('content');
                 $contentMediaItems[] = [
                     'content_id' => $content->id,
                     'media_type' => $request->content_type,
-                    'media_url' => $this->uploadMedia($request->content, 'document'),
+                    'media_url' => $this->uploadMedia($contentFiles, 'document'),
                 ];
             } elseif ($request->content_type == 0) {
                 $contentFiles = $request->file('content');
-                Log::info($contentFiles);
-                Log::info($request);
                 foreach ($contentFiles as $media) {
                     $contentMediaItems[] = [
                         'content_id' => $content->id,
