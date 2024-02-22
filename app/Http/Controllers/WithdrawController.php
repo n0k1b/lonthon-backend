f41710b0249081da25b4b35c6d8a2334ae89ca63
nokib-baybit <nokib@baybit.com>
2024-02-22 13:46:57 +0600

    Request error solve

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\creatorTransactionDetail;
 use App\Models\WithdrawRequest;
-use AWS\CRT\HTTP\Request;
+use Illuminate\Http\Request;
 
 class WithdrawController extends Controller
 {
